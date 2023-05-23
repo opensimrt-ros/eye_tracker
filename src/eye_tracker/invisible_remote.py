a05c901d80f35729becbfc4e2fb22d257804891b
frekle <frekle@kth.se>
2023-05-23 11:55:01 +0000

    wrong colours but connects

--- src/eye_tracker/invisible_remote.py
+++ src/eye_tracker/invisible_remote.py
@@ -21,17 +21,32 @@ import time
 from datetime import datetime
 import matplotlib.pyplot as plt
 import cv2
+import rospy
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+rospy.init_node("eye")
 
-scene_sample, gaze_sample = device.receive_matched_scene_video_frame_and_gaze()
-
-dt_gaze = datetime.fromtimestamp(gaze_sample.timestamp_unix_seconds)
-dt_scene = datetime.fromtimestamp(scene_sample.timestamp_unix_seconds)
-print(f"This gaze sample was recorded at {dt_gaze}")
-print(f"This scene video was recorded at {dt_scene}")
-print(f"Temporal difference between both is {abs(gaze_sample.timestamp_unix_seconds - scene_sample.timestamp_unix_seconds) * 1000:.1f} ms")
-
-scene_image_rgb = cv2.cvtColor(scene_sample.bgr_pixels, cv2.COLOR_BGR2RGB)
-plt.figure(figsize=(10, 10))
-plt.imshow(scene_image_rgb)
-plt.scatter(gaze_sample.x, gaze_sample.y, s=200, facecolors='none', edgecolors='r')
-plt.show(block=True)
+bridge = CvBridge()
+image_pub = rospy.Publisher("image_topic", Image)
+#dt_gaze = datetime.fromtimestamp(gaze_sample.timestamp_unix_seconds)
+#dt_scene = datetime.fromtimestamp(scene_sample.timestamp_unix_seconds)
+#print(f"This gaze sample was recorded at {dt_gaze}")
+#print(f"This scene video was recorded at {dt_scene}")
+#print(f"Temporal difference between both is {abs(gaze_sample.timestamp_unix_seconds - scene_sample.timestamp_unix_seconds) * 1000:.1f} ms")
+while not rospy.is_shutdown():
+    start = time.time()
+    scene_sample, gaze_sample = device.receive_matched_scene_video_frame_and_gaze()
+    scene_image_rgb = cv2.cvtColor(scene_sample.bgr_pixels, cv2.COLOR_BGR2RGB)
+    try:
+        #image_message = bridge.cv2_to_imgmsg(scene_image_rgb, "passthrough")
+        image_message = bridge.cv2_to_imgmsg(scene_sample, "passthrough")
+    except CvBridgeError as e:
+        print(e)
+    image_pub.publish(image_message)
+    stop = time.time()
+    print(stop-start)
+    #cv2.imshow("preview", scene_image_rgb)
+#plt.figure(figsize=(10, 10))
+#plt.imshow(scene_image_rgb)
+#plt.scatter(gaze_sample.x, gaze_sample.y, s=200, facecolors='none', edgecolors='r')
+#plt.show(block=True)
