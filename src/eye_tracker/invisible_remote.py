bd776b505e6f7038ae8ef508a40294994c9b669c
frederico-klein <frekle@kth.se>
2023-05-23 14:37:01 +0200

    unchecked improvements

--- src/eye_tracker/invisible_remote.py
+++ src/eye_tracker/invisible_remote.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
-
-# The two lines below are only needed to execute this code in a Jupyter Notebook
+import cv2
+import rospy
+from sensor_msgs.msg import Image, RegionOfInterest
+from cv_bridge import CvBridge, CvBridgeError
+from sys import exit
 import nest_asyncio
-nest_asyncio.apply()
-
 from pupil_labs.realtime_api.simple import Device, discover_one_device
-#device = discover_one_device()
+
+rospy.init_node("eye")
+
+nest_asyncio.apply()
+#TODO: params? maybe someone wants to know this. 
 device = Device(address="192.168.1.101", port="8080")
 
 print(f"Phone IP address: {device.phone_ip}")
@@ -16,53 +21,39 @@ print(f"Battery level: {device.battery_level_percent}%")
 print(f"Free storage: {device.memory_num_free_bytes / 1024**3:.1f} GB")
 print(f"Serial number of connected glasses: {device.serial_number_glasses}")
 
-import time
-
-from datetime import datetime
-import matplotlib.pyplot as plt
-import cv2
-import rospy
-from sensor_msgs.msg import Image
-from cv_bridge import CvBridge, CvBridgeError
-rospy.init_node("eye")
-
 bridge = CvBridge()
-image_pub = rospy.Publisher("image_topic", Image)
-image_overlay_pub = rospy.Publisher("image_overlay_topic", Image)
-#dt_gaze = datetime.fromtimestamp(gaze_sample.timestamp_unix_seconds)
-#dt_scene = datetime.fromtimestamp(scene_sample.timestamp_unix_seconds)
-#print(f"This gaze sample was recorded at {dt_gaze}")
-#print(f"This scene video was recorded at {dt_scene}")
-#print(f"Temporal difference between both is {abs(gaze_sample.timestamp_unix_seconds - scene_sample.timestamp_unix_seconds) * 1000:.1f} ms")
-from sys import exit
+image_pub = rospy.Publisher("image_topic", Image, queue_size=1)
+image_overlay_pub = rospy.Publisher("image_overlay_topic", Image, queue_size=1)
+gaze_pub = rospy.Publisher("gaze", RegionOfInterest, queue_size=1)
+gaze_size = 20
+
+rate = rospy.Rate(60)
+
+#TODO: node is not shutting down as expected. 
 print("use ctrl-z to quit")
+
 while not rospy.is_shutdown():
     try:
-        #start = time.time()
         scene_sample, gaze_sample = device.receive_matched_scene_video_frame_and_gaze()
-        #scene_image_rgb = cv2.cvtColor(scene_sample.bgr_pixels, cv2.COLOR_BGR2RGB)
         try:
-            #image_message = bridge.cv2_to_imgmsg(scene_image_rgb, "passthrough")
             image_message = bridge.cv2_to_imgmsg(scene_sample.bgr_pixels, "passthrough")
         except CvBridgeError as e:
             print(e)
             break
         image_pub.publish(image_message)
-        image = cv2.circle(scene_sample.bgr_pixels, (int(gaze_sample.x), int(gaze_sample.y)), 20, (0,0,255), 4)
+        image = cv2.circle(scene_sample.bgr_pixels, (int(gaze_sample.x), int(gaze_sample.y)), gaze_size, (0,0,255), 4)
         try:
-            #image_message = bridge.cv2_to_imgmsg(scene_image_rgb, "passthrough")
             image_message = bridge.cv2_to_imgmsg(image, "passthrough")
         except CvBridgeError as e:
             print(e)
             break
         image_overlay_pub.publish(image_message)
-        
-        #stop = time.time()
-        #print(stop-start)
+        gaze_msg = RegionOfInterest()
+        gaze_msg.x_offset = gaze_sample.x - gaze_size/2
+        gaze_msg.y_offset = gaze_sample.y - gaze_size/2
+        gaze_msg.height = gaze_size
+        gaze_msg.width = gaze_size
+        gaze_pub.publish(gaze_msg)
+        rate.sleep() ## maybe this helps break the node?
     except KeyboardInterrupt:
         exit()
-    #cv2.imshow("preview", scene_image_rgb)
-#plt.figure(figsize=(10, 10))
-#plt.imshow(scene_image_rgb)
-#plt.scatter(gaze_sample.x, gaze_sample.y, s=200, facecolors='none', edgecolors='r')
-#plt.show(block=True)
